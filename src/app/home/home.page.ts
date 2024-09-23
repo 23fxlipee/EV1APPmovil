fab57c3f4adbd99a1bf740a8d0976ef7668f6858
Felipe <fe.arces@duocuc.cl>
2024-09-22 21:58:03 -0300

    se inicia pagina de aplicacion movil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    username: string | undefined;
+    password: string | undefined;
+  
+    constructor(private navCtrl: NavController) {}
+  
+    onSubmit() {
+      if (this.username === 'Usuario1' && this.password === 'MiClav3') {
+        this.navCtrl.navigateForward('/home');
+      } else {
+        alert('Usuario o clave incorrectos');
+      }
+    }
+  
+    resetPassword() {
+      alert('Correo de recuperación enviado');
+    }
+  }
 
-  constructor() {}
-
-}
